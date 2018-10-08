0e8dc8acc7ef3741fa457736b562836a7b25021f
Rodolphe Peccatte <rodolphe_peccatte@hotmail.com>
2018-10-09 01:53:55 +0200

    add minimum content for CardsActivity
    I made the CardsActivity class inherit from AppCompatActivity class and overflowed the onCreate method to attach the matching layout.
    I also declared the activity in the manifest.

--- app/src/main/java/com/gothor/werefolfhelper/CardsActivity.java
+++ app/src/main/java/com/gothor/werefolfhelper/CardsActivity.java
@@ -1,4 +1,15 @@
 package com.gothor.werefolfhelper;
 
-public class CardsActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class CardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_cards);
+    }
 }
