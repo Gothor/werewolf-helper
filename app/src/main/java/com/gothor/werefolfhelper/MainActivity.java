73c9896c5a563baf4c179976093d21c0ccc76479
oTelle <oliv.telle@gmail.com>
2018-10-14 19:07:11 +0200

    PlayActivity + Navigation depuis main vers carte, joueurs et partie

--- app/src/main/java/com/gothor/werefolfhelper/MainActivity.java
+++ app/src/main/java/com/gothor/werefolfhelper/MainActivity.java
@@ -6,23 +6,47 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
+
+    Button bt_cards, bt_players, bt_play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        ConstraintLayout layout = (ConstraintLayout) ConstraintLayout.inflate(this, R.layout.activity_main, null);
+        /*ConstraintLayout layout = (ConstraintLayout) ConstraintLayout.inflate(this, R.layout.activity_main, null);*/
 
-        setContentView(layout);
+        bt_cards = (Button) findViewById(R.id.bt_cards);
+        bt_players = (Button) findViewById(R.id.bt_players);
+        bt_play = (Button) findViewById(R.id.bt_play);
 
-        layout.setOnClickListener(this);
-    }
+        bt_cards.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CardsActivity.class);
+                startActivity(intent);
+            }
+        });
 
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, PlayersActivity.class);
-        startActivity(intent);
+        bt_players.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlayersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bt_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /* A FAIRE : tester si nb cartes et nb joueurs sont compatibles
+                avant de lancer le jeu */
+                Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/gothor/werefolfhelper/PlayActivity.java
+++ app/src/main/java/com/gothor/werefolfhelper/PlayActivity.java
@@ -3,16 +3,13 @@ package com.gothor.werefolfhelper;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
 
-public class CardsActivity extends AppCompatActivity {
-
-    Button werewolves_minus, werewolves_plus, bt_start;
+public class PlayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_cards);
+        setContentView(R.layout.activity_play);
     }
 }
