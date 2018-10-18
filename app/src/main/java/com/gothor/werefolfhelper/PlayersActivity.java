d72a5c2b561679fb4dbdc164c697c0072fafdc84
Rodolphe Peccatte <rodolphe_peccatte@hotmail.com>
2018-10-19 01:19:36 +0200

    Add listeners for create and submit button in PlayerActivity

--- app/src/main/java/com/gothor/werefolfhelper/PlayersActivity.java
+++ app/src/main/java/com/gothor/werefolfhelper/PlayersActivity.java
@@ -1,43 +1,93 @@
 package com.gothor.werefolfhelper;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class PlayersActivity extends AppCompatActivity {
 
-    Game game;
+    private Game game;
 
-    RecyclerView recyclerView;
+    private RecyclerView recyclerView;
+    private Button createButton;
+    private Button importButton;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        game = Game.getGame(this);
-
         setContentView(R.layout.activity_players);
 
-        Button startButton = (Button) findViewById(R.id.startButton);
-
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onStartButtonClick();
-            }
-        });
+        game = Game.getGame(this);
 
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        createButton = (Button) findViewById(R.id.bt_create);
+        importButton = (Button) findViewById(R.id.bt_import);
+        submitButton = (Button) findViewById(R.id.bt_start);
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         RecyclerView.Adapter adapter = new PlayerAdapter(game.getPlayers(), PlayerAdapter.NAME_VIEW);
         recyclerView.setAdapter(adapter);
+
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCreateButtonClick();
+            }
+        });
+        importButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onImportButtonClick();
+            }
+        });
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSubmitButtonClick();
+            }
+        });
     }
 
-    protected void onStartButtonClick() {
+    private void onCreateButtonClick() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View layout = inflater.inflate(R.layout.dialog_get_name, null);
 
+        final EditText nameEditText = layout.findViewById(R.id.nameEditText);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(layout)
+                .setPositiveButton("Ajouter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String name = nameEditText.getText().toString();
+                        game.addPlayer(new Player(name));
+                    }
+                })
+                .setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void onImportButtonClick() {
+    }
+
+    protected void onSubmitButtonClick() {
+        onBackPressed();
+        game.save();
     }
 }
